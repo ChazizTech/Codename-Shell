252ea33272a99f3d329f2b58dcb5417352183f42
PF94 <pf94@cloudsoftware.cf>
2020-01-06 19:43:59 -0500

    minor stuff

--- CodenameShell/CodenameShell/Taskbar.Designer.cs
+++ CodenameShell/CodenameShell/Taskbar.Designer.cs
@@ -30,56 +30,94 @@
         {
             this.components = new System.ComponentModel.Container();
             this.start = new System.Windows.Forms.Button();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.programsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.insertProgramsHereLaterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contextMenuStrip1.SuspendLayout();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taskbarRightClick = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.taskManagerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startMenu.SuspendLayout();
+            this.taskbarRightClick.SuspendLayout();
             this.SuspendLayout();
             // 
             // start
             // 
-            this.start.ContextMenuStrip = this.contextMenuStrip1;
+            this.start.BackColor = System.Drawing.SystemColors.Desktop;
             this.start.Location = new System.Drawing.Point(2, 3);
             this.start.Name = "start";
-            this.start.Size = new System.Drawing.Size(48, 25);
+            this.start.Size = new System.Drawing.Size(54, 25);
             this.start.TabIndex = 0;
             this.start.Text = "Start";
-            this.start.UseVisualStyleBackColor = true;
+            this.start.UseVisualStyleBackColor = false;
             this.start.Click += new System.EventHandler(this.Start_Click);
             // 
-            // contextMenuStrip1
+            // startMenu
             // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.programsToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(120, 26);
+            this.startMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.runToolStripMenuItem});
+            this.startMenu.Name = "contextMenuStrip1";
+            this.startMenu.Size = new System.Drawing.Size(181, 76);
             // 
             // programsToolStripMenuItem
             // 
             this.programsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.insertProgramsHereLaterToolStripMenuItem});
             this.programsToolStripMenuItem.Name = "programsToolStripMenuItem";
-            this.programsToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
-            this.programsToolStripMenuItem.Text = "Programs";
+            this.programsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.programsToolStripMenuItem.Text = "&Programs";
+            this.programsToolStripMenuItem.Click += new System.EventHandler(this.ProgramsToolStripMenuItem_Click);
             // 
             // insertProgramsHereLaterToolStripMenuItem
             // 
+            this.insertProgramsHereLaterToolStripMenuItem.DropDown = this.taskbarRightClick;
+            this.insertProgramsHereLaterToolStripMenuItem.Enabled = false;
             this.insertProgramsHereLaterToolStripMenuItem.Name = "insertProgramsHereLaterToolStripMenuItem";
             this.insertProgramsHereLaterToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
             this.insertProgramsHereLaterToolStripMenuItem.Text = "*insert programs here later*";
+            this.insertProgramsHereLaterToolStripMenuItem.Paint += new System.Windows.Forms.PaintEventHandler(this.InsertProgramsHereLaterToolStripMenuItem_Paint);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(116, 6);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.runToolStripMenuItem.Text = "&Run";
+            // 
+            // taskbarRightClick
+            // 
+            this.taskbarRightClick.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.taskManagerToolStripMenuItem});
+            this.taskbarRightClick.Name = "taskbarRightClick";
+            this.taskbarRightClick.Size = new System.Drawing.Size(142, 26);
+            // 
+            // taskManagerToolStripMenuItem
+            // 
+            this.taskManagerToolStripMenuItem.Name = "taskManagerToolStripMenuItem";
+            this.taskManagerToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.taskManagerToolStripMenuItem.Text = "Task Manager";
+            this.taskManagerToolStripMenuItem.Click += new System.EventHandler(this.TaskManagerToolStripMenuItem_Click);
             // 
             // Shell
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 28);
+            this.ClientSize = new System.Drawing.Size(711, 30);
+            this.ContextMenuStrip = this.taskbarRightClick;
             this.Controls.Add(this.start);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.ImeMode = System.Windows.Forms.ImeMode.On;
             this.Name = "Shell";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Taskbar";
-            this.contextMenuStrip1.ResumeLayout(false);
+            this.startMenu.ResumeLayout(false);
+            this.taskbarRightClick.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -87,9 +125,13 @@
         #endregion
 
         private System.Windows.Forms.Button start;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip startMenu;
         private System.Windows.Forms.ToolStripMenuItem programsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem insertProgramsHereLaterToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip taskbarRightClick;
+        private System.Windows.Forms.ToolStripMenuItem taskManagerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
     }
 }
 
--- CodenameShell/CodenameShell/Taskbar.cs
+++ CodenameShell/CodenameShell/Taskbar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics; //<--- for "Process Start"
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,12 +18,25 @@ namespace CodenameShell
         }
 
         private void Start_Click(object sender, EventArgs e)
-        {
+        { //that felling when it doesn't give any errors :D
             Point btnPosition = PointToScreen(this.start.Location);
-
             Point startPoint = new Point(btnPosition.X, btnPosition.Y + this.start.Height);
+            this.startMenu.Show(startPoint);
+        }
 
-            this.contextMenuStrip1.Show(startPoint);
+        private void TaskManagerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("taskmgr.exe");
+        }
+
+        private void ProgramsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
+        }
+
+        private void InsertProgramsHereLaterToolStripMenuItem_Paint(object sender, PaintEventArgs e)
+        {
+            Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
         }
     }
 }
